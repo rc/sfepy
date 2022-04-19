aea47b5a202263898168bd1a266fc64dbdeed07b
Robert Cimrman <cimrman3@ntc.zcu.cz>
2022-04-19 15:17:18 +0200

    WIP

--- tests/test_linalg.py
+++ tests/test_linalg.py
@@ -1,86 +1,77 @@
-from __future__ import absolute_import
-from sfepy.base.testing import TestCommon
+def test_tensors(self):
+    import numpy as nm
+    from sfepy.linalg import dot_sequences, insert_strided_axis
 
-class Test(TestCommon):
+    ok = True
 
-    @staticmethod
-    def from_conf(conf, options):
-        return Test(conf=conf, options=options)
+    a = nm.arange(1, 10).reshape(3, 3)
+    b = nm.arange(9, 0, -1).reshape(3, 3)
 
-    def test_tensors(self):
-        import numpy as nm
-        from sfepy.linalg import dot_sequences, insert_strided_axis
+    dab = nm.dot(a, b)
+    dabt = nm.dot(a, b.T)
+    datb = nm.dot(a.T, b)
+    datbt = nm.dot(a.T, b.T)
 
-        ok = True
+    sa = insert_strided_axis(a, 0, 10)
+    sb = insert_strided_axis(b, 0, 10)
 
-        a = nm.arange(1, 10).reshape(3, 3)
-        b = nm.arange(9, 0, -1).reshape(3, 3)
+    dsab = dot_sequences(sa, sb, mode='AB')
+    _ok = nm.allclose(dab[None, ...], dsab, rtol=0.0, atol=1e-14)
+    self.report('dot_sequences AB: %s' % _ok)
+    ok = ok and _ok
 
-        dab = nm.dot(a, b)
-        dabt = nm.dot(a, b.T)
-        datb = nm.dot(a.T, b)
-        datbt = nm.dot(a.T, b.T)
+    dsabt = dot_sequences(sa, sb, mode='ABT')
+    _ok = nm.allclose(dabt[None, ...], dsabt, rtol=0.0, atol=1e-14)
+    self.report('dot_sequences ABT: %s' % _ok)
+    ok = ok and _ok
 
-        sa = insert_strided_axis(a, 0, 10)
-        sb = insert_strided_axis(b, 0, 10)
+    dsatb = dot_sequences(sa, sb, mode='ATB')
+    _ok = nm.allclose(datb[None, ...], dsatb, rtol=0.0, atol=1e-14)
+    self.report('dot_sequences ATB: %s' % _ok)
+    ok = ok and _ok
 
-        dsab = dot_sequences(sa, sb, mode='AB')
-        _ok = nm.allclose(dab[None, ...], dsab, rtol=0.0, atol=1e-14)
-        self.report('dot_sequences AB: %s' % _ok)
-        ok = ok and _ok
+    dsatbt = dot_sequences(sa, sb, mode='ATBT')
+    _ok = nm.allclose(datbt[None, ...], dsatbt, rtol=0.0, atol=1e-14)
+    self.report('dot_sequences ATBT: %s' % _ok)
+    ok = ok and _ok
 
-        dsabt = dot_sequences(sa, sb, mode='ABT')
-        _ok = nm.allclose(dabt[None, ...], dsabt, rtol=0.0, atol=1e-14)
-        self.report('dot_sequences ABT: %s' % _ok)
-        ok = ok and _ok
+    assert ok
 
-        dsatb = dot_sequences(sa, sb, mode='ATB')
-        _ok = nm.allclose(datb[None, ...], dsatb, rtol=0.0, atol=1e-14)
-        self.report('dot_sequences ATB: %s' % _ok)
-        ok = ok and _ok
+def test_unique_rows(self):
+    import numpy as nm
+    from sfepy.linalg import unique_rows
 
-        dsatbt = dot_sequences(sa, sb, mode='ATBT')
-        _ok = nm.allclose(datbt[None, ...], dsatbt, rtol=0.0, atol=1e-14)
-        self.report('dot_sequences ATBT: %s' % _ok)
-        ok = ok and _ok
+    a = nm.arange(1, 10).reshape(3, 3)
 
-        return ok
+    b = nm.r_[a, a]
+    c = unique_rows(b)
 
-    def test_unique_rows(self):
-        import numpy as nm
-        from sfepy.linalg import unique_rows
+    ok = (a == c).all()
 
-        a = nm.arange(1, 10).reshape(3, 3)
+    assert ok
 
-        b = nm.r_[a, a]
-        c = unique_rows(b)
+def test_assemble1d(self):
+    import numpy as nm
+    from sfepy.linalg import assemble1d
 
-        ok = (a == c).all()
+    a = nm.arange(5)
+    b = nm.arange(2)
 
-        return ok
+    assemble1d(b, [1, 1, 1, 1, 0, 0], a[[0, 2, 3, 4, 1, 1]])
 
-    def test_assemble1d(self):
-        import numpy as nm
-        from sfepy.linalg import assemble1d
+    ok = (b == [2, 10]).all()
 
-        a = nm.arange(5)
-        b = nm.arange(2)
+    assert ok
 
-        assemble1d(b, [1, 1, 1, 1, 0, 0], a[[0, 2, 3, 4, 1, 1]])
+def test_geometry(self):
+    import numpy as nm
+    from sfepy.linalg import get_face_areas
 
-        ok = (b == [2, 10]).all()
+    a1 = get_face_areas([[0, 1, 2, 3]],
+                        [[0, 0], [1, 0], [1, 1], [0, 1]])
 
-        return ok
+    a2 = get_face_areas([[0, 1, 2, 3]],
+                        [[0, 0, 2], [1, 0, 2], [1, 1, 2], [0, 1, 2]])
+    ok = nm.allclose([a1, a2], [1, 1], rtol=0, atol=1e-15)
 
-    def test_geometry(self):
-        import numpy as nm
-        from sfepy.linalg import get_face_areas
-
-        a1 = get_face_areas([[0, 1, 2, 3]],
-                            [[0, 0], [1, 0], [1, 1], [0, 1]])
-
-        a2 = get_face_areas([[0, 1, 2, 3]],
-                            [[0, 0, 2], [1, 0, 2], [1, 1, 2], [0, 1, 2]])
-        ok = nm.allclose([a1, a2], [1, 1], rtol=0, atol=1e-15)
-
-        return ok
+    assert ok
